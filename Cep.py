140266009320770ee20f20c4a15926ba2d4a22b0
GOmoraes <85969891+GOmoraes@users.noreply.github.com>
2021-06-15 21:50:58 -0300

    Add files via upload

---
+++ Cep.py
@@ -0,0 +1,25 @@
+import requests
+from requests.api import options
+def main():
+    Cep = input ('Digite o CEP que deseja consultar:\n')
+
+    if len(Cep) != 8:
+        print('Esse CEP parece estar errado.')
+        main()
+    R= requests.get('http://viacep.com.br/ws/{}/json/'.format(Cep))
+
+    F = R.json()
+    if 'erro' not in F:
+        print ('CEP: {}'.format(F['cep']))
+        print ('UF: {}'.format(F['uf']))
+        print ('Localidade: {}'.format(F['localidade']))
+        print ('Bairro: {}'.format(F['bairro']))
+        exit()
+    else:
+        option = int(input('{} CEP não encontrado, tente novamente! \n1. Tentar novamente \n2 Sair \n'))
+        if option == 1:
+            main()
+        else:
+                print ('Volte Logo')
+if __name__=='__main__':
+    main()
